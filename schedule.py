d183e5e9a64e2ce422e397318d484800c4ef3a14
Ila <ila.ghoshal@mail.mcgill.ca>
2018-09-29 13:47:23 -0400

    update

--- schedule.py
+++ schedule.py
@@ -3,10 +3,19 @@ int points
 bool timeForBreak
 int freeTime 
 bool takeBreak 
+int threshold 
+bool thresh 
 
 #should I take a break?
-if !timeForBreak
-	takeBreak = false 
-	
+if timeForBreak = False 
+	takeBreak = False 
+else if timeForBreak = True 
+	if thresh = True 
+		takeBreak = False 
+	else 
+		takeBreak = True
+		
+
+
 
 	
