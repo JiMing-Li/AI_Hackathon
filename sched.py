82bcd7e3dd5b477f8e742ca2c6f233a86adcef1a
ilaghoshal <36463226+ilaghoshal@users.noreply.github.com>
2018-09-30 06:30:51 -0400

    Add files via upload

--- sched.py
+++ sched.py
@@ -1,53 +1,112 @@
+import random 
+import datetime
+
+testLessThanOne = 0
+testMoreThanOne = 0
+ThuOrFri = 0
+moreThanOne = 0
+testNoThuOrFri = 0 
+testBreak = 0
+testNoThuOrFri = 0 
+testBreak = 0
+testNoThuOrFri = 0
+takeBreak = 0
+testNoBreak = 0
+testThuOrFri = 0
+list_rand_hour = [1,0,0,1,0,0,1,1,0,0,0,0,0,1,0,1,1,1,0,0,1,1,1,0,1,1,0,0,1,1,1,0,1,0,1,1,0,0,0,0,0,1,1,0,1,1,1,0,1,0,1,0,1,1,0,1,0,1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,1,1,0,1,1,0,0,0,1,1,1,1,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,1,1,0,0,1,1,1,0,0,0,0,0,0,1,1,1,1,0,1,1,1,0,1,1,1,0,1,0,1,0,0,0,0,1,0,0,1,0,1,1,1,0,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,1,1,1,0,1,0,0,1,1,1,0,1,0,0,1,1,1,1,0,0,0,0,0,0,1,1,0,1,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,1,1,1,1,0,0,1,1,0,1,0,0,0,1,1,1,0,0,1,0,1,1,1,0,1,0,0,1,0,1,0,0,1,0,0,0,1,1,0,1,0,1,0,1,0,1,1,0,0,1,1,1,1,1,1,0,1,1,0,1,0,1,0,0,1,0,1,1,1,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,1,0,0,0,1,1,0,0,0,1,1,0,0,1,0,0,1,1,1,1,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,1,1,1,1,0,0,0,1,0,0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1,1,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,0,1,1,1,1,1,0,0,1,0,1,1,0,1,1,0,0,1,0,0,0,0,0,0,0,1,1,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0,0,0,1,0,1,1,1,0,0,0,0,0,1,1,0,1,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0]
+list_rand_ampm = [1,1,1,1,1,1,0,1,0,0,1,1,1,0,1,0,0,0,1,1,1,1,0,1,1,1,1,0,1,1,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,1,0,0,1,0,1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1,1,1,1,0,0,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,0,1,1,0,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,0,1,1,1,0,0,0,1,1,1,0,1,0,1,1,0,1,1,0,1,1,1,1,0,0,1,0,1,1,0,0,0,1,1,0,1,1,0,1,1,1,1,0,1,0,0,1,0,1,0,1,0,1,0,1,0,0,0,1,0,1,1,1,1,1,1,0,1,1,0,0,1,1,0,1,1,0,1,1,0,0,1,0,0,0,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,0,1,1,0,0,1,1,1,0,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,1,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,1,0,0,0,1,0,1,0,1,1,0,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,1,0,1,0,1,1,1,0,0,0,1,0,1,1,0,1,0,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,1,1,0,0,0,1,1,1,1,1,0,0,1,0,0,0,1,1,0,1,1,1,1,0,0,1,0,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,1,0,1,1,1,0,1,0,1,1,0,1,0,0,0,0,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,0,1,1,0,1,1,1,1,1,0,0,0,1,0,0,1,0,0,1,0,1,1,1,0,0,1,0,1,1,0,0]
+list_rand_break = [1,0,0,1,1,1,0,1,0,1,1,1,1,1,1,1,0,0,1,1,0,1,0,1,1,1,1,0,1,1,1,1,0,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1,0,1,0,1,0,1,0,1,1,0,1,1,1,1,1,0,1,0,0,1,1,1,0,1,0,1,1,0,1,0,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,0,1,1,0,0,1,1,0,1,0,0,1,0,1,1,1,0,0,1,0,0,1,1,0,1,0,1,1,1,1,1,1,0,0,1,0,0,1,0,1,1,0,0,1,0,0,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,0,1,1,0,1,0,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,0,1,0,1,0,1,0,1,1,1,1,1,1,0,1,0,1,1,1,0,1,0,0,1,0,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0,1,1,0,1,1,0,0,0,0,0,1,1,1,0,1,0,0,0,1,0,1,0,1,1,0,1,0,1,0,0,1,1,1,1,0,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,0,1,1,0,0,1,1,1,0,0,1,1,0,1,1,1,0,1,0,1,1,1,1,0,1,0,0,1,0,1,1,1,1,1,1,0,1,0,1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,0,1,1,1,0,0,1,1,1,1,0,1,1,0,0,1,1,1,1,0,0,1,0,1,1,1,1,1,0,1,1,1,0,1,1,1,0,1,0,1,0,1,0,0,1,0,0,0,1,1,1,0,0,1,0,1,1,1,1,0,1,0,1,1,0,1,1,1,1,0,1,1,0,1,0,1,1,1,1,0,1,0,0,1,0,0,1,1,1,0,0,1,1]
 #declare variables 
-bool takeBreak 
-bool moreThanOne
-bool ThuOrFri
-int probTakeBreak = 0.75
-int probNoTake = 0.25
-int probHourBreak = (2/3)
-int probNoBreakHour = (1/3) 
-int probNoHourBreak = 1
-int probNoBreakNoHour = 0
-int probTFBreak = 0.5
-int probTFNoBreak = 0.5
-int probNoTFBreak = 0.5
-int probNoTFNoBreak = 0.5
-int probBreak
-int probNoBreak
-str outcome 
- 
+testTotal = 100000
+testHourAndBreak = 0
+testNoHourAndBreak = 0
+testHourAndNoBreak = 0
+testNoBreakAndNoHour = 0
+testTFAndBreak = 0
+testNoTFAndBreak = 0
+testNoTFAndBreak = 0
+testNoTFAndNoBreak = 0
+
+
+#run test cases 
+for i in range(len(list_rand_break)):
+	if (list_rand_break[i] == 1):
+		testBreak = testBreak + 1
+	else:
+		testNoBreak = testNoBreak + 1
+for j in range(len(list_rand_hour)):
+	if (list_rand_break[i] == 1):
+		testMoreThanOne = testMoreThanOne + 1
+	else:
+		testLessThanOne = testLessThanOne + 1
+for k in range(len(list_rand_ampm)):
+	if (list_rand_ampm[k] == 1):
+		testThuOrFri = testThuOrFri + 1
+	else:
+		testNoThuOrFri = testNoThuOrFri + 1
+for m in range(len(list_rand_ampm)):
+ 	if (list_rand_break[m]==1 and list_rand_break[m]==1):
+ 		testHourAndBreak = testHourAndBreak+1
+ 	elif (list_rand_break[m]==0 and list_rand_break[m]==1):
+ 		testNoHourAndBreak = testNoHourAndBreak+1
+ 	elif (list_rand_break[m]==1 and list_rand_break[m]==0):
+ 		testHourAndNoBreak = testHourAndNoBreak+1
+ 	else:
+ 		testNoBreakAndNoHour = testNoBreakAndNoHour+1
+for n in range(len(list_rand_ampm)):
+	if (list_rand_ampm[n]==1 and list_rand_break[n]==1):
+ 		testTFAndBreak = testTFAndBreak+1
+	elif (list_rand_ampm[n]==0 and list_rand_break[n]==1):
+ 		testNoTFAndBreak = testNoTFAndBreak+1
+	elif (list_rand_ampm[n]==1 and list_rand_break[n]==0):
+ 		testNoTFAndBreak = testNoTFAndBreak+1
+	else:
+ 		testNoTFAndNoBreak = testNoTFAndNoBreak+1 
+
+#declare more 
+probTakeBreak = testBreak/testTotal 
+probNoTake = testNoBreak/testTotal
+probHourBreak = testHourAndBreak/testBreak 
+probNoBreakHour = testHourAndNoBreak/testBreak
+probNoHourBreak = testNoHourAndBreak/testBreak
+probNoBreakNoHour = testNoBreakAndNoHour/testNoBreak
+probTFBreak = testTFAndBreak/testBreak
+probTFNoBreak = testNoTFAndNoBreak/testNoBreak
+probNoTFBreak = testNoTFAndBreak/testBreak
+probNoTFNoBreak = testNoTFAndNoBreak/testBreak
+
 #calculate prediction 
-if moreThanOne = True 
-	if ThuOrFri = True 
+if moreThanOne == 1: 
+	if ThuOrFri == 1: 
 		probBreak = probHourBreak * probThuOrFri * probTakeBreak
 		probNoBreak = probNoBreakHour * probTFNoBreak * probNoBreak 
-		if (probBreak > probNoBreak)
-			outcome = "break"
-		else 
-			outcome = "don't take a break"
+		if probBreak > probNoBreak:
+			outcome = "1"
+		else: 
+			outcome = "0"
 		
-	else 
+	else :
 		probBreak = probHourBreak * probNoTFBreak * probTakeBreak
 		probNoBreak = probNoBreakHour * probNoTFNoBreak * probNoTake
-		if (probBreak > probNoBreak)
-			outcome = "break"
-		else 
-			outcome = "don't take a break"
-else 
-	if ThuOrFri = True 
+		if probBreak > probNoBreak:
+			outcome = "1"
+		else:
+			outcome = "0"
+else:
+	if ThuOrFri == 1: 
 		probBreak = probNoHourBreak* probThuOrFri * probTakeBreak
 		probNoBreak = probNoBreakNoHour * probTFNoBreak * probNoTake 
-		if (probBreak > probNoBreak)
-			outcome = "break"
-		else 
-			outcome = "don't take a break"
-	else 
+		if probBreak > probNoBreak:
+			outcome = "1"
+		else :
+			outcome = "0"
+	else :
 		probBreak = probNoHourBreak * probNoTFNoBreak * probTakeBreak
 		probNoBreak = probNoBreakHour * probNoTFNoBreak * probNoTake
-		if (probBreak > probNoBreak)
-			outcome = "break"
-		else 
-			outcome = "don't take a break"
+		if probBreak > probNoBreak:
+			outcome = "1"
+		else:
+			outcome = "0"
 
-
+ print(outcome, datetime.datetime.now())
 
