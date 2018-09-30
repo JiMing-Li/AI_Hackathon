6ca0b52fab480e643fa3db5b0aa1d38e11a9a6d6
ilaghoshal <36463226+ilaghoshal@users.noreply.github.com>
2018-09-30 10:54:44 -0400

    Add files via upload

--- regression.py
+++ regression.py
@@ -14,38 +14,59 @@ while data[n, 2] == "Study":
 
 
 #fake data
-x = np.asarray(data[0:n, 0], dtype = float)
+x = np.asarray(data[0:n, 0:1], dtype = float)
 y = np.asarray(data[0:n, 1], dtype = float)
+poly=PolynomialFeatures(degree=4)
+poly_x=poly.fit_transform(x)
+regressor=LinearRegression()
+regressor.fit(poly_x,y)
+for x in range(6, 23):
+	print( x, regressor.predict(poly.fit_transform(x)), "Study")
+#plt.scatter(x,y,color='red')
+#plt.plot(x,regressor.predict(poly.fit_transform(x)),color='blue')
+#plt.show()
 #x_new = np.linspace(x[0], x[-1], num=len(x)*10)
 
-z = np.polyfit(x, y, 4)
-print(z)
+#z = np.polyfit(x, y, 4)
+#print(z)
 
 m=n
 while data[m, 2] == "Sport":
 	m+=1
 
 #fake data
-x2 = np.asarray(data[n:m, 0], dtype = float)
+x2 = np.asarray(data[n:m, 0:1], dtype = float)
 y2 = np.asarray(data[n:m, 1], dtype = float)
 #x_new = np.linspace(x[0], x[-1], num=len(x)*10)
-
-a = np.polyfit(x2, y2, 4)
-print(a)
+poly2=PolynomialFeatures(degree=4)
+poly_x2=poly2.fit_transform(x2)
+regressor2=LinearRegression()
+regressor2.fit(poly_x2,y2)
+for x2 in range(6, 23):
+	print( x2, regressor2.predict(poly2.fit_transform(x2)), "Sport")
+#a = np.polyfit(x2, y2, 4)
+#print(a)
 
 o=m
-while data[(o), 2] == "Leisure":
+while data[o, 2] == "Leisure":
 	o+=1
+	
 	if o>= len(data):
 		break
 
 #fake data
-x3 = np.asarray(data[m:(o), 0], dtype = float)
-y3 = np.asarray(data[m:(o), 1], dtype = float)
+x3 = np.asarray(data[m:o, 0:1], dtype = float)
+y3 = np.asarray(data[m:o, 1], dtype = float)
 #x_new = np.linspace(x[0], x[-1], num=len(x)*10)
 
-b = np.polyfit(x3, y3, 4)
-print(b)
+poly3=PolynomialFeatures(degree=4)
+poly_x3=poly3.fit_transform(x3)
+regressor3=LinearRegression()
+regressor3.fit(poly_x3,y3)
+for x3 in range(6, 23):
+	print( x3, regressor3.predict(poly3.fit_transform(x3)), "Leisure")
+#b = np.polyfit(x3, y3, 4)
+#print(b)
 
 
 	#print(coefs)
